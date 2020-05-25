c4005375dc3c1e2d98a42c5ed85ce6063c41bdc0
Žiga Ranzinger <coyzib3@gmail.com>
2020-05-25 20:00:59 +0200

    dodali smo json vkljucitev v model.py

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 STEVILO_DOVOLJENIH_NAPAK = 10
 
@@ -109,16 +110,20 @@ class Vislice:
             return max(self.igre.keys()) + 1 #len(self.igre.keys())
 
     def nova_igra(self):
+        self.preberi_iz_datoteke()
         #dobimo svež id
         nov_id = self.prosti_id_igre()
         #naredimo novo igro
         sveza_igra = nova_igra()
         #vse to shranimo v self.igre
         self.igre[nov_id] = (sveza_igra, ZACETEK)
-        #vrnemo id
+        #vrnemo id 
+        self.shrani_v_datoteko()
         return nov_id
 
+
     def ugibaj(self, id_igre, crka):
+        self.preberi_iz_datoteke()
         #Dobimo staro igro ven
         trenutna_igra, _ = self.igre[id_igre]
 
@@ -127,4 +132,20 @@ class Vislice:
 
         #Zapišemo posodobljeno stanje in igro nazaj v "BAZO"
         self.igre[id_igre] = (trenutna_igra,novo_stanje)
+        self.shrani_v_datoteko()
 
+    def shrani_v_datoteko(self):
+        igre = {}
+        for id_igre, (igra, stanje) in self.igre.items(): #id_igre, (Igra, stanje)
+            igre[id_igre] = ((igra.geslo, igra.crke), stanje)
+        
+        with open('Vislice3/stanje_iger.json', 'w') as out_file:
+            json.dump(igre, out_file)
+    
+    def preberi_iz_datoteke(self):
+        with open('Vislice3/stanje_iger.json', 'r') as in_file:
+            igre = json.load(in_file) #igre_iz_diska
+        
+        self.igre = {}
+        for id_igre, ((geslo, crke), stanje) in igre.items():
+            self.igre[int(id_igre)] = Igra(geslo, crke), stanje
